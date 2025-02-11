224ee65404a23a2c148fc23192795dd8fae0c259
letsouzza <leticia.sa1907@gmail.com>
2025-02-11 17:23:00 -0300

    Atualizações- Não concluído

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 "use strict" /*ajudar a manter a boa prática e não ter erros */
 
-const botaoTrocarImagem = document.getElementById('trocar-img')
-const imagem = document.getElementById('figura')
 // getElementById significa escolher um elemento pelo id (#) dele
+const botaoTrocarImagem = document.getElementById('trocar-img')
+
+const fotos = {
+    foto1: "var(--imagem1)",
+    foto2: "var(--imagem2)",
+    foto3: "var(--imagem3)",
+    foto4: "var(--imagem4)",
+    foto5: "var(--imagem5)"
+};
 
 function trocaImg () {
-    figura.src = './img/fatima.webp';
+    imagem.src = fotos.foto1;
+    document.documentElement.style.setProperty('--img-fundo', img)
 }
 
 botaoTrocarImagem.addEventListener('click', trocaImg)
